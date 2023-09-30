f05bd387ad951b200e77187c101c2d34d0adbf4f
Ruth Goh <ruth.goh@kcl.ac.uk>
2023-09-30 13:25:51 +0100

    Solve exercise and pass all pytest tests

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes < 1:
+        raise ValueError
+    list = [2]
+    number_done = 1
+    current_num = 3
+    while number_done < number_of_primes:
+        prime = True
+        for p in list:
+            if current_num%p == 0:
+                prime = False
+                break
+        
+        if prime:
+            list.append(current_num)
+            number_done += 1
+        
+        current_num += 1
     return list
